ae62512959670077693c39901c2779f60069163b
Andrey Paramonov <github@ndpar.com>
2016-09-28 08:54:20 -0400

    Template HTML

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl: 'dashboard.component.html'
 })
 export class DashboardComponent { }
