69cc8c1fdc850ae49fe79129c88b5197fb7666f4
Andrey Paramonov <github@ndpar.com>
2016-09-28 10:23:28 -0400

    Route with parameter

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
 
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 @Component({
@@ -12,10 +16,28 @@ import { Hero } from './hero';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
+      <button (click)="goBack()">Back</button>
     </div>
   `
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input()
   hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id).then(hero => this.hero = hero);
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,8 +5,13 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
     //return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000)).then(() => HEROES);
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
   }
 }
