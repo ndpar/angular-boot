3a155a5be3142cb247378cdd25004f8989b541e7
Andrey Paramonov <github@ndpar.com>
2016-09-28 14:53:46 -0400

    Search component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,5 @@
+import './rxjs-extensions';
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
@@ -10,6 +12,7 @@ import { InMemoryDataService }  from './in-memory-data.service';
 import { AppComponent }        from './app.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 
@@ -27,6 +30,7 @@ import { routing } from './app.routing';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
+    HeroSearchComponent,
     HeroesComponent
   ],
   providers: [ HeroService ],
