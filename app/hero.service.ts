6ccaf351fdb54ad25a959722dfbe4f5a8ab18e55
Andrey Paramonov <github@ndpar.com>
2016-09-28 13:38:41 -0400

    GET data from remote service

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,14 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
 
+  private heroesUrl = 'app/heroes';  // URL to web api
+
+  constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
-    //return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000)).then(() => HEROES);
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
   getHero(id: number): Promise<Hero> {
