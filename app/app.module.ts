a7e40a2df8db3562825fdbaf67789006b0b6c6f3
Andrey Paramonov <github@ndpar.com>
2016-09-28 08:39:14 -0400

    DashboardComponent mock

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
@@ -17,6 +18,7 @@ import { routing } from './app.routing';
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,9 +1,14 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 
 const appRoutes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
   {
     path: 'heroes',
     component: HeroesComponent
