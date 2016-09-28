267621e0e0fc68978e94ef210c65bf55790bfcff
Andrey Paramonov <github@ndpar.com>
2016-09-28 11:47:40 -0400

    Implement HeroesComponent::gotoDetail

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -14,7 +14,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {
+  }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -26,5 +29,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
