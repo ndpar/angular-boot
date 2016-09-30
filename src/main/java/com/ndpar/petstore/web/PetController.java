9a46d5ea12fed2820bad95c0023c6952dbfc7b9e
Andrey Paramonov <github@ndpar.com>
2016-09-29 23:38:15 -0400

    PetController calls PetDao

--- src/main/java/com/ndpar/petstore/web/PetController.java
+++ src/main/java/com/ndpar/petstore/web/PetController.java
@@ -1,21 +1,56 @@
 package com.ndpar.petstore.web;
 
+import com.ndpar.petstore.dao.PetDao;
 import com.ndpar.petstore.domain.Pet;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/app")
 public class PetController {
 
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private PetDao dao;
+
     @ResponseBody
-    @RequestMapping("/pet/{petId}")
+    @RequestMapping("/pets")
+    List<Pet> getPets() {
+        log.info("Get all pets");
+        return dao.getAllPets();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/pet", method = RequestMethod.POST)
+    public void create(@RequestBody Pet pet) {
+        log.info("Create: {}", pet);
+        dao.create(pet);
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/pet/{petId}")
     Pet getPetById(@PathVariable Long petId) {
-        Pet pet = new Pet();
-        pet.setId(petId);
-        pet.setName("Doggie");
-        return pet;
+        log.info("Get by Id: {}", petId);
+        return dao.getPetById(petId);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path = "/pet", method = RequestMethod.PUT)
+    public void update(@RequestBody Pet pet) {
+        log.info("Update: {}", pet);
+        dao.update(pet);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path = "/pet/{petId}", method = RequestMethod.DELETE)
+    void delete(@PathVariable Long petId) {
+        log.info("Delete: {}", petId);
+        dao.delete(petId);
     }
 }
