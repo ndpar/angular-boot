ddc9431a10f8655e2a30cdfc8ed461ebb7df838e
Andrey Paramonov <github@ndpar.com>
2018-03-13 14:34:01 -0400

    asciidoc

--- server/src/test/groovy/com/ndpar/petstore/web/PetControllerTest.groovy
+++ server/src/test/groovy/com/ndpar/petstore/web/PetControllerTest.groovy
@@ -2,18 +2,29 @@ package com.ndpar.petstore.web
 
 import com.ndpar.petstore.dao.PetDao
 import com.ndpar.petstore.domain.Pet
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.restdocs.JUnitRestDocumentation
+import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
 import static org.mockito.BDDMockito.given
 import static org.mockito.Mockito.*
 import static org.springframework.http.MediaType.APPLICATION_JSON
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*
+import static org.springframework.restdocs.payload.PayloadDocumentation.*
+import static org.springframework.restdocs.request.RequestDocumentation.*
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -21,11 +32,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(PetController.class)
 class PetControllerTest {
 
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets")
+
     @Autowired
-    MockMvc mvc
+    WebApplicationContext context
 
     @MockBean
     PetDao dao
+
+    MockMvc mvc
+
+    @Before
+    void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
+                .build()
+    }
 
     static path(String endPoint) {
         '/api' + endPoint
@@ -53,6 +76,14 @@ class PetControllerTest {
         mvc.perform(get(path('/pet/?name=e')).accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string('[{"id":1,"name":"Premier"},{"id":2,"name":"Seconde"}]'))
+                .andDo(document('get-pet', preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                requestParameters(
+                        parameterWithName('name').optional().description('Part of the name')
+                ),
+                responseFields([
+                        fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("Pet's ID"),
+                        fieldWithPath("[].name").type(JsonFieldType.STRING).description("Pet's name")
+                ])))
     }
 
     @Test
@@ -62,6 +93,14 @@ class PetControllerTest {
         mvc.perform(post(path('/pet')).contentType(APPLICATION_JSON).content('{"name":"Test"}'))
                 .andExpect(status().isCreated())
                 .andExpect(content().string('{"id":1,"name":"Test"}'))
+                .andDo(document('post-pet', preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                requestFields(
+                        fieldWithPath("name").type(JsonFieldType.STRING).description("Pet's name")
+                ),
+                responseFields([
+                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("Pet's ID"),
+                        fieldWithPath("name").type(JsonFieldType.STRING).description("Pet's name")
+                ])))
     }
 
     @Test
@@ -75,14 +114,22 @@ class PetControllerTest {
     void get_pet_1_200() {
         given(dao.getPetById(1)).willReturn(new Pet(id: 1, name: 'Test'))
 
-        mvc.perform(get(path('/pet/1')).accept(APPLICATION_JSON))
+        mvc.perform(get(path('/pet/{id}'), 1).accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string('{"id":1,"name":"Test"}'))
+                .andDo(document('get-pet-by-id', preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                pathParameters(
+                        parameterWithName('id').description("Pet's ID")
+                ),
+                responseFields([
+                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("Pet's ID"),
+                        fieldWithPath("name").type(JsonFieldType.STRING).description("Pet's name")
+                ])))
     }
 
     @Test
     void get_pet_1_400() {
-        mvc.perform(get(path('/pet/ABC')).accept(APPLICATION_JSON))
+        mvc.perform(get(path('/pet/ABC')))
                 .andExpect(status().isBadRequest())
     }
 
@@ -90,7 +137,7 @@ class PetControllerTest {
     void get_pet_1_404() {
         given(dao.getPetById(1)).willReturn(null)
 
-        mvc.perform(get(path('/pet/1')).accept(APPLICATION_JSON))
+        mvc.perform(get(path('/pet/1')))
                 .andExpect(status().isBadRequest())
     }
 
@@ -100,6 +147,11 @@ class PetControllerTest {
 
         mvc.perform(put(path('/pet')).contentType(APPLICATION_JSON).content('{"id":1,"name":"Test"}'))
                 .andExpect(status().isOk())
+                .andDo(document('put-pet', preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                requestFields(
+                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("Pet's ID"),
+                        fieldWithPath("name").type(JsonFieldType.STRING).description("Pet's name")
+                )))
     }
 
     @Test
@@ -119,7 +171,12 @@ class PetControllerTest {
     @Test
     void delete_pet_1_200() {
         given(dao.delete(1)).willReturn(1)
-        mvc.perform(delete(path('/pet/1'))).andExpect(status().isOk())
+        mvc.perform(delete(path('/pet/{id}'), 1))
+                .andExpect(status().isOk())
+                .andDo(document('delete-pet', preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                pathParameters(
+                        parameterWithName('id').description("Pet's ID")
+                )))
     }
 
     @Test
