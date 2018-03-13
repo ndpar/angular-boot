0cc9c5c54581a821269cc67bb7c73b5c298bd2b9
Andrey Paramonov <github@ndpar.com>
2018-03-13 10:33:27 -0400

    Fix unit tests broken by Swagger

--- server/src/test/groovy/com/ndpar/petstore/dao/PetDaoTest.groovy
+++ server/src/test/groovy/com/ndpar/petstore/dao/PetDaoTest.groovy
@@ -5,14 +5,14 @@ import com.ndpar.petstore.domain.Pet
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = Application.class)
+@SpringBootTest(classes = Application.class)
 class PetDaoTest {
 
     @Autowired
